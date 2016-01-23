c4fb7cc63b6a69eb49231a8c3d2cb19bc75e5216
niji <sweetandsourstrawberry@gmail.com>
2016-01-23 17:10:32 +0900

    issue #1 プロジェクトを追加。

---
+++ MockingjaySample/MockingjaySample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MockingjaySample
+//
+//  Created by niji on 2016/01/23.
+//  Copyright © 2016年 niji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
